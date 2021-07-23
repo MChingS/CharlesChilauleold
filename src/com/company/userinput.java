435776ab1de71a9c50120925dcc5b011cc49dbd2
mchingis <53611127+MChingS@users.noreply.github.com>
2021-07-23 20:44:30 +0200

    java

--- src/com/company/userinput.java
+++ src/com/company/userinput.java
@@ -29,28 +29,32 @@ public class userinput {
                 temp = df.format(Double.valueOf(val));
 
                 int pos = temp.indexOf(".");
-
-                if(pos>0)
-
+                System.out.println("The dot is in "+pos+"th position");
+                if(pos > 0) {
                     System.out.println("Your double is :" + df.format(num));
-                {String num1 = temp.substring(0, pos);
+                    {
+                        String num1 = temp.substring(0, pos);
 
-                String num2 = temp.substring(pos + 1, temp.length());
-                pos = pos + 1;
-                System.out.println("Dot is in position " + pos);
-                System.out.println("First part is " + num1);
-                System.out.println("Second part is " + num2);
-                int temp1 = Integer.valueOf(num1);
-                int temp2 = Integer.valueOf(num2);
-                int sum = temp1 + temp2;
+                        String num2 = temp.substring(pos + 1, temp.length());
+                        pos = pos + 1;
+                        System.out.println("Dot is in position " + pos);
+                        System.out.println("First part is " + num1);
+                        System.out.println("Second part is " + num2);
+                        int temp1 = Integer.valueOf(num1);
+                        int temp2 = Integer.valueOf(num2);
+                        int sum = temp1 + temp2;
 
-                if (temp2 == temp1) {
-                    System.out.println("Identical");
+                        if (temp2 == temp1) {
+                            System.out.println("Identical");
 
-                } else {
-                    System.out.println("Not Identical");
+                        } else {
+                            System.out.println("Not Identical");
+                        }
+                        System.out.println("The sun is :" + sum);
+                    }
                 }
-                System.out.println("The sun is :" + sum);
+                else {
+                    System.out.println("Number is not a double");
                 }
             }
             else {
